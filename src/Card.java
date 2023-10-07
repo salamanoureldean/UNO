4f90702caa064d75c34f320ecba97d4eed177347
salama <salamanoureldean2001@gmail.com>
2023-10-07 14:22:58 -0400

    version 1.0 of card class

---
+++ src/Card.java
@@ -0,0 +1,37 @@
+import java.util.Random;
+
+public class Card {
+    public enum Value{ZERO, ONE, TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE, REVERSE, SKIP, WILD, WILD_DRAW_TWO_CARDS};
+    public enum Color{RED, GREEN, BLUE, YELLOW};
+
+    private final Value value;
+    private final Color color;
+
+    //Constructs a random card from the initialized Value and Color enums.
+    public Card(Color color, Value value) {
+        Random random = new Random();
+        this.color = Color.values()[random.nextInt(Color.values().length)];
+        this.value = Value.values()[random.nextInt(Value.values().length)];
+    }
+
+    //Returns the randomized color of the card.
+    public Color getColor(){
+        return color;
+    }
+
+    //Returns the randomized value of the card.
+    public Value getValue(){
+        return value;
+    }
+
+    //Returns a string representation of the card. Returns value and color if the value of the card is not a wildcard.
+    //Returns value of the card only if card is a wildcard since color is irrelevant.
+    public String stringCard(){
+        if(value == Value.WILD){
+            return "A" + value;
+        }
+        else{
+            return "A" + color + value;
+        }
+    }
+}
