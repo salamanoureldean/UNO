cdddc285143635026a391315d8e5e95a9d03a3c4
Mahad Toukaleh <mahad.toukaleh56@gmail.com>
2023-10-08 22:24:33 -0400

    Create Player class version 1.0 and tested it in the Main class.

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,30 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Scanner scanner = new Scanner(System.in);
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+        // Ask the user for the number of players
+        System.out.print("Enter the number of players: ");
+        int numPlayers = scanner.nextInt();
+        scanner.nextLine(); // Consume newline
 
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
+        // Create an ArrayList to hold the players
+        ArrayList<Player> players = new ArrayList<>();
+
+        // Create players
+        for (int i = 0; i < numPlayers; i++) {
+            Player player = new Player();
+            players.add(player);
+        }
+
+        // Display each player's hand
+        for (int i = 0; i < numPlayers; i++) {
+            System.out.println("\nPlayer " + (i + 1) + "'s Hand:");
+            players.get(i).viewHand();
         }
     }
 }
---
+++ src/Player.java
@@ -0,0 +1,104 @@
+/**
+ * Represents a player in the Uno card game.
+ * @version 1.0
+ */
+public class Player {
+    private Hand hand;      // A player's hand made up of cards
+    private int score;      // Player's score
+    private boolean unoCalled;  // Indicates whether Uno has been called by the player
+
+    /**
+     * Constructs a new player with an empty hand, a score of 0, and Uno not called.
+     */
+    public Player() {
+        this.hand = new Hand(); // Initialize the player's hand
+        this.score = 0;        // Initialize the player's score
+        this.unoCalled = false; // Uno is initially not called
+    }
+
+    /**
+     * Gets the player's current score.
+     *
+     * @return The player's score.
+     */
+    public int getScore() {
+        return score;
+    }
+
+    /**
+     * Sets the player's score.
+     *
+     * @param score The new score to set.
+     */
+    public void setScore(int score) {
+        this.score = score;
+    }
+
+    /**
+     * Checks if Uno has been called by the player.
+     *
+     * @return True if Uno has been called; otherwise, false.
+     */
+    public boolean getUnoCalled() {
+        return unoCalled;
+    }
+
+    /**
+     * Sets the Uno call status for the player.
+     *
+     * @param unoCalled True to indicate Uno has been called; otherwise, false.
+     */
+    public void setUnoCalled(boolean unoCalled) {
+        this.unoCalled = unoCalled;
+    }
+
+    /**
+     * Draws a card and adds it to the player's hand.
+     */
+    public void drawCard() {
+        hand.addCard();
+    }
+
+    /**
+     * Plays a card from the player's hand.
+     *
+     * @param card The card to play.
+     */
+    public void playCard(Card card) {
+        hand.removeCard(card);
+    }
+
+    /**
+     * Displays all cards in the player's hand.
+     */
+    public void viewHand() {
+        hand.printAll();
+    }
+
+    /**
+     * Checks if the player has Uno (one card left in hand).
+     *
+     * @return True if the player has Uno; otherwise, false.
+     */
+    public boolean hasUno() {
+        return hand.getCardNum() == 1;
+    }
+
+    /**
+     * Calls Uno, indicating that the player has one card left in hand.
+     */
+    public void callUno() {
+        unoCalled = true;
+    }
+
+    /**
+     * Updates the player's score based on whether they have won the round.
+     *
+     * @param hasWon True if the player has won the round; otherwise, false.
+     */
+    public void updateScore(boolean hasWon) {
+        if (hasWon) {
+            score += 1; // Increase the score by 1 if the player has won.
+        }
+    }
+}
