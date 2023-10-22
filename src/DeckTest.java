28cbd31a92261beb3f8d2a42733fafebb035a08e
Spysock <abodymajeed11@gmail.com>
2023-10-22 11:06:34 -0400

    -Added comments splitting work -Added DeckTest

---
+++ src/DeckTest.java
@@ -0,0 +1,6 @@
+public class DeckTest {
+    /**
+     * Worker: Abody
+     * Task: test ordering based off enum, compare size of the deck to set size, test draw and place functions based off sizes not content
+     */
+}
--- src/PlayerTest.java
+++ src/PlayerTest.java
@@ -1,2 +1,9 @@
 public class PlayerTest {
+    /**
+     * Worker: Mahad
+     * Tasks: getters, setters (Make sure these don't break), PlayCard (size), Draw Card (size), hasUNO (testing it someway)
+     *
+     * Worker #2: Pietro
+     * Tasks: test any and all updateScore functionality
+     */
 }
