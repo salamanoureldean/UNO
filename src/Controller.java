dfb7b2500617bb30cde6b279be043f3e6229cc1e
Spysock <abodymajeed11@gmail.com>
2023-11-06 18:03:06 -0500

    -Adding the controller class -Next update will comment in work we must do

---
+++ src/Controller.java
@@ -0,0 +1,11 @@
+import java.awt.event.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+public class Controller implements ActionListener{
+    Game game = new Game();
+
+    public void actionPerformed(ActionEvent e){
+
+    }
+
+}
